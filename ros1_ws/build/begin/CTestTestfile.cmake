91471f490958a2789a6d86cd0959224f27b64c64
Adithya1911 <adithya.c19112003@gmail.com>
2024-08-18 11:00:39 +0530

    creating the package is done

---
+++ ros1_ws/build/begin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adithya/ROS/Ros/ROS/ros1_ws/src/begin
+# Build directory: /home/adithya/ROS/Ros/ROS/ros1_ws/build/begin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- ros1_ws/build/catkin_generated/setup_cached.sh
+++ ros1_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/adithya/ROS/Ros/ROS/ros1_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/adithya/ROS/Ros/ROS/ros1_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/adithya/ROS/Ros/ROS/ros1_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/adithya/ROS/Ros/ROS/ros1_ws/src:$ROS_PACKAGE_PATH"
