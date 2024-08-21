eeb40589e1dd89ab3856ddced5e16a33df7cee58
Adithya1911 <adithya.c19112003@gmail.com>
2024-08-21 17:26:23 +0530

    gazebo was added

--- ros1_ws/build/cmake_install.cmake
+++ ros1_ws/build/cmake_install.cmake
@@ -131,6 +131,9 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/adithya/ROS/Ros/ROS/ros1_ws/build/gtest/cmake_install.cmake")
+  include("/home/adithya/ROS/Ros/ROS/ros1_ws/build/turtlebot3_simulations/turtlebot3_simulations/cmake_install.cmake")
+  include("/home/adithya/ROS/Ros/ROS/ros1_ws/build/turtlebot3_simulations/turtlebot3_fake/cmake_install.cmake")
+  include("/home/adithya/ROS/Ros/ROS/ros1_ws/build/turtlebot3_simulations/turtlebot3_gazebo/cmake_install.cmake")
   include("/home/adithya/ROS/Ros/ROS/ros1_ws/build/begin/cmake_install.cmake")
 
 endif()
